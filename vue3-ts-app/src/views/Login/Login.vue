547d16886ea7568bffd17388f4b1515e65fec621
Hailie17 <192151452@qq.com>
2024-07-17 23:54:06 +0800

    登录页面布局

--- vue3-ts-app/src/views/Login/Login.vue
+++ vue3-ts-app/src/views/Login/Login.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="login">
-    <h2>login</h2>
+    <div class="header">
+      <span class="header-logo">
+        <i class="iconfont icon-vue"></i>
+        <i class="iconfont icon-icon-test"></i>
+        <i class="iconfont icon-typescript"></i>
+      </span>
+      <div class="header-title">在线考勤系统</div>
+    </div>
+    <div class="desc">零基础入门到进阶，掌握三大热门技术系统</div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import http from '@/utils/http'
-// http
-//   .post('/user/login', {
-//     email: 'huangrong@imooc.com',
-//     pass: 'huangrong'
-//   })
-//   .then((res) => {
-//     console.log(res, 'res')
-//   })
-
-import { useStore } from '@/stores'
-const store = useStore()
-store.dispatch('users/login').then((res) => {
-  console.log(res.data)
-})
+// import { useStore } from '@/stores'
+// const store = useStore()
+// store.dispatch('users/login').then((res) => {
+//   console.log(res.data)
+// })
 </script>
 
 <style lang="scss" scoped>
 .login {
+  width: 100vw;
+  height: 100vh;
+  background: url('@/assets/images/login-bg.svg') no-repeat;
+  background-size: 100%;
+  .header-logo {
+  }
 }
 </style>
