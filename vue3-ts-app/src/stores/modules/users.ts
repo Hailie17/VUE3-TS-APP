7bc60fe9c3200d1f4710cea9d0a640a106f3969a
Hailie17 <192151452@qq.com>
2024-07-17 22:19:20 +0800

    状态管理

--- vue3-ts-app/src/stores/modules/users.ts
+++ vue3-ts-app/src/stores/modules/users.ts
@@ -1,3 +1,4 @@
+import http from '@/utils/http'
 import type { State } from '../index'
 import type { ActionTree, MutationTree, GetterTree } from 'vuex/types/index.js'
 
@@ -6,7 +7,11 @@ export interface UsersState {}
 const state: UsersState = {}
 
 const mutations: MutationTree<UsersState> = {}
-const actions: ActionTree<UsersState, State> = {}
+const actions: ActionTree<UsersState, State> = {
+  login() {
+    return http.post('/user/login')
+  }
+}
 const getters: GetterTree<UsersState, State> = {}
 
 export default {
--- vue3-ts-app/src/views/Login/Login.vue
+++ vue3-ts-app/src/views/Login/Login.vue
@@ -5,15 +5,21 @@
 </template>
 
 <script setup lang="ts">
-import http from '@/utils/http'
-http
-  .post('/user/login', {
-    email: 'huangrong@imooc.com',
-    pass: 'huangrong'
-  })
-  .then((res) => {
-    console.log(res, 'res')
-  })
+// import http from '@/utils/http'
+// http
+//   .post('/user/login', {
+//     email: 'huangrong@imooc.com',
+//     pass: 'huangrong'
+//   })
+//   .then((res) => {
+//     console.log(res, 'res')
+//   })
+
+import { useStore } from '@/stores'
+const store = useStore()
+store.dispatch('users/login').then((res) => {
+  console.log(res.data)
+})
 </script>
 
 <style lang="scss" scoped>
