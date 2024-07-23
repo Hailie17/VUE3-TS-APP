7f8fc4fd2828f255eb9d4e75a621f55cfe9781b7
Hailie17 <192151452@qq.com>
2024-07-23 23:34:45 +0800

    home-aside的布局

--- vue3-ts-app/src/views/Home/components/HomeAside.vue
+++ vue3-ts-app/src/views/Home/components/HomeAside.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="homeMain">
-    <h2>homeaside</h2>
-  </div>
+  <el-menu default-active="1-1" class="el-menu-vertical-demo">
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="1-1"
+          ><el-icon><location /></el-icon>item one</el-menu-item
+        >
+        <el-menu-item index="1-2"
+          ><el-icon><location /></el-icon>item two</el-menu-item
+        >
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="1-3"
+          ><el-icon><location /></el-icon>item three</el-menu-item
+        >
+      </el-menu-item-group>
+    </el-sub-menu>
+  </el-menu>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-.homeMain {
+.el-menu {
+  height: calc(100% - 60px);
+  border: none;
+  padding-top: 30px;
+}
+.el-menu-item.is-active {
+  background: #e6f7ff;
+  border-right: 3px solid #1890ff;
 }
 </style>
