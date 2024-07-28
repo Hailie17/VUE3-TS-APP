1c13ee7f89f69c52ccc313948a8119af187fac69
Hailie17 <192151452@qq.com>
2024-07-29 00:21:43 +0800

    12-7 添加考勤页面布局

--- vue3-ts-app/src/views/Execption/Execption.vue
+++ vue3-ts-app/src/views/Execption/Execption.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="exw">
-    <h2>execption</h2>
+  <div class="exception-title">
+    <el-button type="primary">异常处理</el-button>
+    <el-space>
+      <el-button plain>2022年</el-button>
+      <el-select>
+        <el-option v-for="item in 12" :key="item" :value="item" :label="item + '月'" />
+      </el-select>
+    </el-space>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-.exw {
+.exception-title {
 }
 </style>
