b79b13794d70f8e6e8436773fbfce8b4e6932986
Hailie17 <192151452@qq.com>
2024-08-13 01:07:35 +0800

    创建异常页面,返回首页，routerlink

--- vue3-ts-app/src/views/NotAuth/NotAuth.vue
+++ vue3-ts-app/src/views/NotAuth/NotAuth.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="status-wrapper">
     <h4>没有权限</h4>
-    <el-button type="primary">回到首页</el-button>
+    <router-link to="/">
+      <el-button type="primary">回到首页</el-button>
+    </router-link>
   </div>
 </template>
 
--- vue3-ts-app/src/views/NotFound/NotFound.vue
+++ vue3-ts-app/src/views/NotFound/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="status-wrapper">
     <h4>页面找不到</h4>
-    <el-button type="primary">回到首页</el-button>
+    <router-link to="/">
+      <el-button type="primary">回到首页</el-button>
+    </router-link>
   </div>
 </template>
 
